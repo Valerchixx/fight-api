812de39b1eed443dee5e20372e4a23de37fa5e47
Valeria Proshachenko <vproshachenko@gmail.com>
2023-12-17 00:57:58 +0200

    add validation on create user

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -3,7 +3,14 @@ import { USER } from "../models/user.js";
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
- next()
+  const keys = Object.keys(USER);
+  keys.shift()
+  const isUserValid = keys.every(key => key in req.body)
+  if(isUserValid && req.body.password.length >= 3) {
+    next();
+  } else {
+    res.sendStatus(400)
+  }
 };
 
 const updateUserValid = (req, res, next) => {
